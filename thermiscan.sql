d5cab61ea01d8a001092090a7f47e6f6061d6ed9
lorenzo.bdcc <lorenzo1227@gmail.com>
2019-12-11 15:55:44 +0100

    Create thermiscan.sql

---
+++ thermiscan.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mer 11 Décembre 2019 à 15:55
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `thermiscan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `camera`
+--
+
+CREATE TABLE IF NOT EXISTS `camera` (
+  `idCamera` int(11) NOT NULL AUTO_INCREMENT,
+  `nomCamera` varchar(100) NOT NULL,
+  `IdUser` int(11) NOT NULL,
+  PRIMARY KEY (`idCamera`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf16 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `IdUser` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `passw` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdUser`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf16 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+CREATE TABLE IF NOT EXISTS `video` (
+  `idVideo` int(11) NOT NULL AUTO_INCREMENT,
+  `dateVideo` date NOT NULL,
+  `nomVideo` varchar(100) NOT NULL,
+  `IdCamera` int(11) NOT NULL,
+  PRIMARY KEY (`idVideo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf16 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
