e025accec85b7f52e69a7eacba50b28a96c63650
Ryser-tom <rysertom@gmail.com>
2020-02-19 15:24:09 +0100

    Modification to work with background

--- pythonApp/background.py
+++ pythonApp/background.py
@@ -1,8 +1,10 @@
 import logging  # To create easy log file
 import time  # For the time.sleep
-import image  # For the image script
+import extract  # For the image script
 #import extract  # For the video script
+import threading
 
+import shutil
 import pytesseract as pyt
 # For the file detection, It doesn't work on 64-bit
 from watchdog.observers import Observer
@@ -19,7 +21,7 @@ logging.basicConfig(filename='\\testPython//app.log',
 #class that wait for an event, when it get's an event it call the Handler class and give him the event
 class Watcher:
     # TODO change the directory to watch for the definitive directory
-    DIRECTORY_TO_WATCH = "\\testPython"
+    DIRECTORY_TO_WATCH = "../web/file/toDo"
 
     def __init__(self):
         self.observer = Observer()
@@ -48,8 +50,26 @@ class Handler(FileSystemEventHandler):
             return None
 
         elif event.event_type == 'created':
-            image.read_break_image(event.src_path)
+            path = event.src_path.split('/')
+            path2 = path[3].split('\\')
+            source = path[0] + "/" + path[1] + "/" + path[2] + "/" + path2[0] + "/" + path2[1]
+            destination = path[0] + "/" + path[1] + "/" + path[2] + "/" + "done"
+            print(path2)
+            print(source)
+
+            thread = threading.Thread(target=extract.extractFrames(event.src_path, source))
+            thread.start()
+
+            # wait here for the result to be available before continuing
+            thread.join()
+            extract.extractFrames(event.src_path, source)
             logging.warning('The file at %s has been added' % event.src_path)
+            print('the CSV is done.')
+            try:
+                shutil.move(source, destination)
+                print('The folder as been moved.')
+            except:
+                print("An exception occurred, cannot move folder")
 
 
 if __name__ == '__main__':
--- pythonApp/extract.py
+++ pythonApp/extract.py
@@ -4,42 +4,43 @@ import os
 import shutil
 import image
 # Read the video from specified path
-cam = cv2.VideoCapture("..//video//vidTest.mp4")
+path = "..//video//vidTest.mp4"
+def extractFrames(path, folderSource):
+    cam = cv2.VideoCapture(path)
+    try:
+        os.unlink('value.csv')
+        # creating a folder named data
+        if not os.path.exists('/data'):
+            os.makedirs('/data')
+            
 
-try:
-    os.unlink('value.csv')
-    # creating a folder named data
-    if not os.path.exists('data'):
-        os.makedirs('data')
-        
+    # if not created then raise error
+    except OSError:
+        print('Error: Creating directory of data')
 
-# if not created then raise error
-except OSError:
-    print('Error: Creating directory of data')
+    # frame
+    currentframe = 0
+    print(cam)
 
-# frame
-currentframe = 0
+    while (True):
 
-while (True):
+        # reading from frame
+        ret, frame = cam.read()
 
-    # reading from frame
-    ret, frame = cam.read()
+        if ret:
+            # if video is still left continue creating images
+            name = './data/frame' + str(currentframe) + '.jpg'
+            #print('Creating...' + name)
 
-    if ret:
-        # if video is still left continue creating images
-        name = './data/frame' + str(currentframe) + '.jpg'
-        #print('Creating...' + name)
+            # writing the extracted images
+            cv2.imwrite(name, frame)
+            image.read_break_image(currentframe, frame, folderSource)
+            # increasing counter so that it will
+            # show how many frames are created
+            currentframe += 1
+        else:
+            break
 
-        # writing the extracted images
-        cv2.imwrite(name, frame)
-        image.read_break_image(currentframe, frame)
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
-    else:
-        break
-
-# Release all space and windows once done
-cam.release()
-cv2.destroyAllWindows()
-shutil.rmtree('data')
+    # Release all space and windows once done
+    cam.release()
+    cv2.destroyAllWindows()
